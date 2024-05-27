efa969842ebe7ae55ba641082e6eea076dc3fbe7
MPanchenko <waildpanik@gmail.com>
2024-05-27 19:04:06 +0300

    update

--- integrationBot/src/helpers/shRunner.ts
+++ integrationBot/src/helpers/shRunner.ts
@@ -1,14 +1,13 @@
 import { spawn } from 'child_process';
 import path from 'path';
 
-const exec = (cmd, args = []) =>
+const exec = (cmd: string, args: string[] = []) =>
   new Promise((resolve, reject) => {
     console.log(`Started: ${cmd} ${args.join(' ')}`);
     const app = spawn(cmd, args, { stdio: 'inherit' });
     app.on('close', (code) => {
       if (code !== 0) {
-        err = new Error(`Invalid status code: ${code}`);
-        err.code = code;
+        const err = new Error(`Invalid status code: ${code}`);
         return reject(err);
       }
       return resolve(code);
@@ -17,7 +16,8 @@ const exec = (cmd, args = []) =>
   });
 
 const run = async (scriptName: string) => {
-  await exec('bash', [path.join(__dirname, `./${scriptName}.sh`)]);
+  const scriptPath = path.join(__dirname, `./${scriptName}.sh`);
+  await exec('bash', [scriptPath]);
 };
 
 export { run };
--- integrationBot/src/slack/index.ts
+++ integrationBot/src/slack/index.ts
@@ -1,4 +1,9 @@
-import { App, KnownBlock } from '@slack/bolt';
+import {
+  App,
+  SlackCommandMiddlewareArgs,
+  AllMiddlewareArgs,
+  KnownBlock,
+} from '@slack/bolt';
 import KubeClientFactory from '../k8s';
 import k8s from '@kubernetes/client-node';
 
@@ -18,83 +23,153 @@ export default class SlackBot {
     this.k8sApi = KubeClientFactory.getClient();
   }
 
+  async helpHandler({
+    command,
+    say,
+    respond,
+  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
+    await respond({
+      text: 'Here are the available commands:',
+      // https://app.slack.com/block-kit-builder
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `/${this.commandPrefix}get`,
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `/${this.commandPrefix}create *[environment-name]* *[source]*`,
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `/${this.commandPrefix}delete *[environment-name]*`,
+          },
+        },
+      ],
+    });
+  }
+
+  async deleteHandler({
+    command,
+    say,
+    respond,
+  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
+    const text = command.text.trim();
+    const [environment] = text.split(' ');
+    await respond({
+      text: 'Here are the available commands:',
+      // https://app.slack.com/block-kit-builder
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `You invoked the /delete command, environment: ${environment} deleted`,
+          },
+        },
+      ],
+    });
+  }
+
+  async createHandler({
+    command,
+    say,
+    respond,
+  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
+    const text = command.text.trim();
+    const [environment, source] = text.split(' ');
+    await respond({
+      text: 'Here are the available commands:',
+      // https://app.slack.com/block-kit-builder
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `You invoked the /create command, environment: ${environment} created from ${source}}`,
+          },
+        },
+      ],
+    });
+  }
+
+  async getHandler({
+    command,
+    ack,
+    say,
+    respond,
+  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
+    try {
+      const namespaces = await this.k8sApi.listNamespace();
+      console.dir(namespaces);
+      // await say(JSON.stringify(namespaces));
+      await respond({
+        text: 'Here are the available namespaces:',
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `You invoked the /get command`,
+            },
+          },
+        ],
+        // https://app.slack.com/block-kit-builder
+        /*blocks: namespaces.body.items.map<KnownBlock>((x): KnownBlock => {
+          return {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `/${x.metadata}`,
+            },
+          };
+        }),*/
+      });
+    } catch (err) {
+      console.error(err);
+      await respond({
+        text: 'Here are the available namespaces:',
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `You invoked the /get command, but we have some error with cluster: ${(<Error>err).message}`,
+            },
+          },
+        ],
+      });
+    }
+  }
+
   async listen(port: number) {
-    this.app.command(
-      `/${this.commandPrefix}help`,
-      async ({ command, ack, say, respond }) => {
-        await ack();
-        await respond({
-          text: 'Here are the available commands:',
-          // https://app.slack.com/block-kit-builder
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `/${this.commandPrefix}get`,
-              },
-            },
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `/${this.commandPrefix}create *[environment-name]* *[source]*`,
-              },
-            },
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `/${this.commandPrefix}delete *[environment-name]*`,
-              },
-            },
-          ],
-        });
-      },
-    );
-
-    this.app.command(
-      `/${this.commandPrefix}get`,
-      async ({ command, ack, say, respond }) => {
-        await ack();
-        try {
-          const namespaces = await this.k8sApi.listNamespace();
-          await say(JSON.stringify(namespaces));
-
-          /*await respond({
-            text: 'Here are list of namespaces:',
-            blocks: namespaces.body.items.map((x): KnownBlock => {
-              return {
-                type: 'section',
-                text: {
-                  type: 'mrkdwn',
-                  text: `${x.kind}: ${x.metadata}: ${x.status}`,
-                },
-              };
-            }),
-          });*/
-        } catch (err) {
-          console.error(err);
-        }
-        await say('You invoked the /get command');
-      },
-    );
-
-    this.app.command(
-      `/${this.commandPrefix}delete`,
-      async ({ command, ack, say }) => {
-        await ack();
-        await say('You invoked the /delete command');
-      },
-    );
-
-    this.app.command(
-      `/${this.commandPrefix}create`,
-      async ({ command, ack, say }) => {
-        await ack();
-        await say('You invoked the /create command');
-      },
-    );
+    const commandName = new RegExp(`/${this.commandPrefix}*`);
+    this.app.command(commandName, async (slackCommandMiddlewareArgs) => {
+      const { command, ack, say, respond } = slackCommandMiddlewareArgs;
+      await ack();
+      console.dir(command);
+      switch (command.command) {
+        case `/${this.commandPrefix}help`:
+          await this.helpHandler(slackCommandMiddlewareArgs);
+          break;
+        case `/${this.commandPrefix}delete`:
+          await this.deleteHandler(slackCommandMiddlewareArgs);
+          break;
+        case `/${this.commandPrefix}get`:
+          await this.getHandler(slackCommandMiddlewareArgs);
+          break;
+        default:
+          say(`Unknown command: ${command.command}, plz check.`);
+      }
+    });
 
     await this.app.start(port);
   }
