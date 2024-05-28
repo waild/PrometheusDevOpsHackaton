b499a1fde0eb8be17ce4d053b87f481e2004c41e
MPanchenko <waildpanik@gmail.com>
2024-05-28 09:08:48 +0300

    load k8s api config from cluster

--- integrationBot/src/k8s/index.ts
+++ integrationBot/src/k8s/index.ts
@@ -3,7 +3,7 @@ import { KubeConfig, CoreV1Api } from '@kubernetes/client-node';
 export default class KubeClientFactory {
   static getClient(): CoreV1Api {
     const kc = new KubeConfig();
-    kc.loadFromDefault();
+    kc.loadFromCluster();
 
     const api = kc.makeApiClient(CoreV1Api);
     return api;
