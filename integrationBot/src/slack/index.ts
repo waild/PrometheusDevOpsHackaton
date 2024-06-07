814b898bd9e60b5c72f446fe6a49d13676d26246
MPanchenko <waildpanik@gmail.com>
2024-06-07 11:58:49 +0300

    add bot args parsing, hide systems ns from list

--- integrationBot/src/slack/index.ts
+++ integrationBot/src/slack/index.ts
@@ -7,12 +7,19 @@ import {
 import KubeClientFactory from '../k8s';
 import { run } from '../helpers/shRunner';
 import k8s from '@kubernetes/client-node';
+import argParser from 'minimist';
 
 export default class SlackBot {
   private app: App;
   private commandPrefix: string;
   private k8sApi: k8s.CoreV1Api;
-
+  private defaultNs = [
+    'kube-system',
+    'kube-public',
+    'kube-node-lease',
+    'default',
+    'flux-system',
+  ];
   constructor() {
     this.commandPrefix = process.env.COMMAND_PREFIX || '';
     this.app = new App({
@@ -58,13 +65,15 @@ export default class SlackBot {
     });
   }
 
-  async deleteHandler({
-    command,
-    say,
-    respond,
-  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
-    const text = command.text.trim();
-    const [environment] = text.split(' ');
+  async deleteHandler(
+    {
+      command,
+      say,
+      respond,
+    }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>,
+    args: argParser.ParsedArgs,
+  ) {
+    const [environment] = args._;
     await run('delete-helm', [environment]);
     await respond({
       text: 'Here are the available commands:',
@@ -81,14 +90,16 @@ export default class SlackBot {
     });
   }
 
-  async createHandler({
-    command,
-    say,
-    respond,
-  }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
-    const text = command.text.trim();
-    const [environment, source] = text.split(' ');
-    await run('create-helm', [environment, source]);
+  async createHandler(
+    {
+      command,
+      say,
+      respond,
+    }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>,
+    args: argParser.ParsedArgs,
+  ) {
+    const [environment, source] = args._;
+    await run('add-helm-release', [environment, source]);
     await respond({
       text: 'Here are the available commands:',
       // https://app.slack.com/block-kit-builder
@@ -111,18 +122,47 @@ export default class SlackBot {
     respond,
   }: SlackCommandMiddlewareArgs & AllMiddlewareArgs<any>) {
     const namespaces = await this.k8sApi.listNamespace();
-    await respond({
-      text: 'Here are the available namespaces:',
-      blocks: namespaces.body.items.map<KnownBlock>((x): KnownBlock => {
-        return {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `/${x.metadata?.name}: ${x.metadata?.annotations}`,
+    const nss = namespaces.body.items
+      .filter((x) => !!x.metadata)
+      .map((x): string => x.metadata?.name || '')
+      .filter((x) => !!x && !this.defaultNs.includes(x));
+
+    if (nss.length > 0) {
+      const resolvedPodInfos = await Promise.all(
+        nss.map((ns) => this.k8sApi.listNamespacedPod(ns)),
+      );
+
+      //await this.k8sApi.listNamespacedService;
+      await respond({
+        text: 'Here are the available namespaces:',
+        blocks: nss.map<KnownBlock>((x, index): KnownBlock => {
+          return {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `environment ${x}: ${resolvedPodInfos[index].body.items.map(
+                (y) => {
+                  return `${y.metadata?.name} - ${y.spec?.containers[0].image}`;
+                },
+              )}`,
+            },
+          };
+        }),
+      });
+    } else {
+      await respond({
+        text: 'Here are the available namespaces:',
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `There no any environments;`,
+            },
           },
-        };
-      }),
-    });
+        ],
+      });
+    }
   }
 
   async listen(port: number) {
@@ -131,16 +171,18 @@ export default class SlackBot {
       const { command, ack, say, respond } = slackCommandMiddlewareArgs;
       await ack();
 
+      const args = argParser(command.text.trim().split(' '));
+
       try {
         switch (command.command) {
           case `/${this.commandPrefix}help`:
             await this.helpHandler(slackCommandMiddlewareArgs);
             break;
           case `/${this.commandPrefix}delete`:
-            await this.deleteHandler(slackCommandMiddlewareArgs);
+            await this.deleteHandler(slackCommandMiddlewareArgs, args);
             break;
           case `/${this.commandPrefix}create`:
-            await this.createHandler(slackCommandMiddlewareArgs);
+            await this.createHandler(slackCommandMiddlewareArgs, args);
             break;
           case `/${this.commandPrefix}get`:
             await this.getHandler(slackCommandMiddlewareArgs);
