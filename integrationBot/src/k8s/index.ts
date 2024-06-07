3df5448d38fc453d519190c8e71a7e7aa31f7299
MPanchenko <waildpanik@gmail.com>
2024-06-07 11:57:19 +0300

    update kubeclient to load config from correct environment

--- integrationBot/src/k8s/index.ts
+++ integrationBot/src/k8s/index.ts
@@ -3,7 +3,11 @@ import { KubeConfig, CoreV1Api } from '@kubernetes/client-node';
 export default class KubeClientFactory {
   static getClient(): CoreV1Api {
     const kc = new KubeConfig();
-    kc.loadFromCluster();
+    if (process.env.KUBERNETES_SERVICE_HOST) {
+      kc.loadFromCluster();
+    } else {
+      kc.loadFromDefault();
+    }
 
     const api = kc.makeApiClient(CoreV1Api);
     return api;
